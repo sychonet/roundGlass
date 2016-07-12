cd0a36e38613bf901ecd5fb0ec6248eedb4e7748
cl-macmini-93 <cl-macmini-93@cl-macmini-93.local>
2016-07-12 19:55:20 +0530

    first commit loop for each line

--- solution.js
+++ solution.js
@@ -1,6 +1,21 @@
 var readline = require('readline');
-var rl = readline.createInterface({
+var rl = readline.createInterface({ // To read data
     input: process.stdin,
     output: process.stdout,
     terminal: false
 });
+var matchingBrackets = {
+    '(': ')',
+    '[': ']',
+    '{': '}'
+};
+var value = "(()((())(){))  ";
+var inputArray = rl.input.split('\n');
+inputArray = inputArray.map(Function.prototype.call, String.prototype.trim);
+var noOfInputs = inputArray.shift();
+var regexMatch = new RegExp("/^[[]{}()]+$/");
+for(var i = 0; i < inputArray.length; i++){
+    //console.log(inputArray[i]);
+    var bracketsMatched = checkBrackets(inputArray[i]);
+    console.log(bracketsMatched == false?"No":bracketsMatched);
+}
